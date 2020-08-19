31c8a588c6a328821872316862f87f1413e85b21
ginnnnnn <aginlo@hotmail.com>
2020-08-19 23:29:36 +0800

    Updates

---
+++ precache-manifest.80aa9feddb551bad31a6ad56e1dce9e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "404f9f7824bbc5d5f825a0064bfc27ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f66c6cbbf7885c6d3c0",
+    "url": "/static/css/main.4eff2831.chunk.css"
+  },
+  {
+    "revision": "4e1119af42d8607286cd",
+    "url": "/static/js/2.6851b256.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.6851b256.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f66c6cbbf7885c6d3c0",
+    "url": "/static/js/main.954d19e1.chunk.js"
+  },
+  {
+    "revision": "a3fd8fcee403f7590641",
+    "url": "/static/js/runtime-main.01277389.js"
+  }
+]);
